0d56f69a12e2f0cc85383c83eb6a10f9b946a7dc
mutchler <mutchler@rose-hulman.edu>
2025-03-20 13:13:02 -0400

    Removed my password

--- src_on_pico_micropython/secrets.py
+++ src_on_pico_micropython/secrets.py
@@ -1,8 +1,8 @@
 secrets = {
-   'ssid' : 'David',
-   'password' : 'engdengd',
-   'mqtt_username' : '',
-   'mqtt_key' : '',
-   'broker' : 'broker.hivemq.com',
-   'port' : 1883
+    "ssid": "YOUR_PHONE_NAME_HERE",
+    "password": "YOUR_PHONE_HOTSPOT_PASSSWORD_HERE",
+    "mqtt_username": "",
+    "mqtt_key": "",
+    "broker": "broker.hivemq.com",
+    "port": 1883,
 }
