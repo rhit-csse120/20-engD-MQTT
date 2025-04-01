adc1c1cd9d27911884ff4d5e7d6c353ded26fde6
mutchler <mutchler@rose-hulman.edu>
2025-04-01 10:06:12 -0400

    Added note re Maximum Compatibility mode for iPhones.

--- src_on_pico/secrets.py
+++ src_on_pico/secrets.py
@@ -1,8 +1,11 @@
 secrets = {
-    "ssid": "YOUR-PHONE-ID",
-    "password": "YOUR-PHONE-PASSWORD",
+    "ssid": "YOUR_PHONE_NAME_HERE",
+    "password": "YOUR_PHONE_HOTSPOT_PASSSWORD_HERE",
     "mqtt_username": "",
     "mqtt_key": "",
     "broker": "broker.emqx.io",
     "port": 1883,
 }
+
+# If using an iPhone, be sure to check (turn on)
+# the Maximum Compatibility option
--- src_on_pico_micropython/secrets.py
+++ src_on_pico_micropython/secrets.py
@@ -3,6 +3,9 @@ secrets = {
     "password": "YOUR_PHONE_HOTSPOT_PASSSWORD_HERE",
     "mqtt_username": "",
     "mqtt_key": "",
-    "broker": "broker.hivemq.com",
+    "broker": "broker.emqx.io",
     "port": 1883,
 }
+
+# If using an iPhone, be sure to check (turn on)
+# the Maximum Compatibility option
